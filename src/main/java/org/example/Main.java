e0ae1636692fb38a35e1cddcd5b46ab23486bf00
devangshrimali99 <2020uee1442@mnit.ac.in>
2024-03-25 02:18:08 +0530

    Implemented Put, Get, and Remove functions for MyHashMap class. Added exception handling to handle cases where the key does not exist or when resizing the table is necessary.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Testing Done!");
+        MyHashMap<Integer, String> map = new MyHashMap<>();
+        map.put(1,"A");
+        map.put(2,"B");
+        map.put(3,"C");
+        map.put(4,"D");
+        map.forEach(entry ->{
+            if(entry.key%2==0){
+                map.remove(entry.key);
+            }
+            else{
+                System.out.println("Key: " + entry.key + ", Value: " + entry.value);
+            }
+        });
     }
 }
---
+++ src/main/java/org/example/MyHashMap.java
@@ -0,0 +1,126 @@
+package org.example;
+
+import java.util.function.Consumer;
+
+public class MyHashMap<K,V> {
+
+    private static final int INITIAL_SIZE =  1<<4;
+    private static final int MAXIMUM_SIZE = 1<<30;
+    private static final double LOAD_FACTOR = 0.75;
+    public static int CURRENT_SIZE=0;
+
+    public Entry[] hashTable;
+
+    class Entry<K,V> {
+        public K key;
+        public V value;
+        public Entry next;
+
+        Entry(K k,V v){
+            key=k;
+            value=v;
+        }
+    }
+
+    public MyHashMap(){
+        hashTable = new Entry[INITIAL_SIZE];
+    }
+
+    public MyHashMap(int capacity){
+        int tableSize = tableSizeFor(capacity);
+        hashTable = new Entry[tableSize];
+    }
+
+    final int tableSizeFor(int cap){
+        int n = cap-1;
+        n=n>>>1;
+        n=n>>>2;
+        n=n>>>4;
+        n=n>>>8;
+        n=n>>>16;
+        return (n<0) ? 1 : (n>=MAXIMUM_SIZE) ? MAXIMUM_SIZE : n+1;
+    }
+
+    private int getHash(K key){
+        return Math.abs(key.hashCode()%hashTable.length);
+    }
+
+    public void put(K key, V value){
+        if(CURRENT_SIZE+1>hashTable.length*LOAD_FACTOR){
+            resize();
+        }
+        int hashCode = getHash(key);
+        Entry node = hashTable[hashCode];
+        if (node == null) {
+            Entry newNode = new Entry(key,value);
+            hashTable[hashCode] = newNode;
+        } else {
+            Entry previousNode = node;
+            while(node!=null){
+                if(node.key==key){
+                    node.value=value;
+                    return;
+                }
+                previousNode=node;
+                node=node.next;
+            }
+            Entry newNode = new Entry(key,value);
+            previousNode.next=newNode;
+        }
+        CURRENT_SIZE++;
+    }
+
+    public V get(K key){
+        int hashCode = getHash(key);
+        Entry node = hashTable[hashCode];
+        while(node!=null){
+            if(node.key==key){
+                return (V) node.value;
+            }
+            node=node.next;
+        }
+        throw new IllegalArgumentException("Key does not exist in the HashMap : " + key);
+    }
+
+    public void remove(K key){
+        int hashCode = getHash(key);
+        Entry node = hashTable[hashCode];
+        Entry prevNode = null;
+        while(node!=null){
+            if(node.key==key){
+                if(prevNode==null){
+                    hashTable[hashCode]=null;
+                }
+                else{
+                    prevNode.next=node.next;
+                }
+                CURRENT_SIZE--;
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Cannot Remove Key from HashMap as Key : " + key + "not found");
+    }
+
+    public void forEach(Consumer<Entry<K, V>> action) {
+        for (Entry<K, V> node : hashTable) {
+            if (node != null) {
+                action.accept(node);
+            }
+        }
+    }
+
+    private void resize(){
+        int newCapacity = hashTable.length*2;
+        if(newCapacity>MAXIMUM_SIZE){
+            throw new IllegalStateException("Cannot add more elements, maximum size reached");
+        }
+        Entry<K,V>[] newHashTable = new Entry[newCapacity];
+        for(Entry<K,V> node: hashTable){
+            if(node!=null){
+                int newHashCode = Math.abs(node.key.hashCode()) % newCapacity;
+                newHashTable[newHashCode] = node;
+            }
+        }
+        hashTable=newHashTable;
+    }
+}
